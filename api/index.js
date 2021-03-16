3a9bb210354e9f50077ead509e5e69b6f0728fcb
windfly <974589985@qq.com>
2021-03-16 09:40:07 +0800

    修改代理接口

--- api/index.js
+++ api/index.js
@@ -1,5 +1,7 @@
-module.exports = (req, res) => {
-  res.json({
-    json: true,
-  });
-};
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+// 导出一个中间件函数
+module.exports = createProxyMiddleware({
+    target: 'http://fullstack.net.cn',
+    changeOrigin: true,
+  })
